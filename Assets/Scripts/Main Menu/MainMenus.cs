2b6b4bb3251a085489525b7141752355cde50452
LutherSH <thedisconnected707@gmail.com>
2025-06-01 14:43:48 +0700

    video start game

--- Assets/Scripts/Main Menu/MainMenus.cs
+++ Assets/Scripts/Main Menu/MainMenus.cs
@@ -1,27 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenus : MonoBehaviour
 {
     public GameObject MenuPanel;
+    public GameObject canvasVideo;
     public GameObject CreditPanel;
     public GameObject OptionPanel;
     public ParticleSystem particleSystem;
+    public VideoPlayer videoPlayer;
     void Start()
     {
         MenuPanel.SetActive(true);
         CreditPanel.SetActive(false);
         OptionPanel.SetActive(false);
+        canvasVideo.SetActive(false);
         // Cek jika di MainMenu, restart partikel
         particleSystem.Play();
+        videoPlayer.Stop();
         //if (SceneManager.GetActiveScene().name == "MainMenu")
         //{
         //    particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            
+
         //}
-        
+
     }
     public void StartStory()
+    {
+        //SceneManager.LoadScene("Games");
+        canvasVideo.SetActive(true);
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    public void OnVideoFinished(VideoPlayer vp)
     {
         SceneManager.LoadScene("Games");
     }
@@ -40,7 +53,7 @@ public class MainMenus : MonoBehaviour
     public void CreditButton()
     {
         MenuPanel.SetActive(false);
-        CreditPanel.SetActive(true );
+        CreditPanel.SetActive(true);
         OptionPanel.SetActive(false);
     }
     public void OptionButton()
@@ -56,5 +69,4 @@ public class MainMenus : MonoBehaviour
         CreditPanel.SetActive(false);
         OptionPanel.SetActive(false);
     }
-
 }
